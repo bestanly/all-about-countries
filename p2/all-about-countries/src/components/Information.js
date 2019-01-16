2f58c62fd1a419a352e777500af743b5db8d68ed
bestanly <ibestanly@gmail.com>
2019-01-16 15:54:33 -0500

    Add all the meaningful information

--- p2/all-about-countries/src/components/Information.js
+++ p2/all-about-countries/src/components/Information.js
@@ -1,15 +1,30 @@
 import React from "react";
 import numberWithCommas from "../funtions/numberWithCommas";
+import InformationElement from "./InformationList";
 const Information = props => (
   <div className="col information">
     <hr />
     <h5 style={{ textAlign: "center" }}>Information:</h5>
     <h6>Name: {props.country.name}</h6>
+    <img src={props.country.flag} alt="flag" width="100%" height="200" />
     <h6>Capital: {props.country.capital}</h6>
     <h6>Region: {props.country.region}</h6>
     <h6>Subregion: {props.country.subregion}</h6>
-    <h6>Population: {numberWithCommas(props.country.population)}</h6>
-    <h6>Calling Code: {props.country.callingCode}</h6>
+    <h6>Demonym: {props.country.demonym}</h6>
+    <h6>
+      Area: {numberWithCommas(props.country.area)} Km<sup>2</sup>
+    </h6>
+    <h6>Population: {numberWithCommas(props.country.population)} people </h6>
+    <h6>Calling Codes:</h6>
+    <InformationElement list={props.country.callingCodes} />
+    <h6>Languages</h6>
+    <InformationElement list={props.country.languages} keyName="name" />
+    <h6>Currency</h6>
+    <InformationElement
+      list={props.country.currencies}
+      keyName="name"
+      keySymbol="symbol"
+    />
   </div>
 );
 export default Information;
---
+++ p2/all-about-countries/src/components/InformationList.js
@@ -0,0 +1,15 @@
+import React from "react";
+const InformationElement = props => {
+  return (
+    <ul>
+      {props.list.map((element, index) => (
+        <li key={index}>
+          {props.keyName ? element[props.keyName] : element}
+          {props.keySymbol ? " (" + element[props.keySymbol] + ")" : ""}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default InformationElement;
